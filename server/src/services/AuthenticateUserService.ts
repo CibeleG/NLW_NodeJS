9dc325d0d09d7f183edae5fc1ad69e7c735d35c0
CibeleG <cibelegoudinho13@gmail.com>
2021-08-21 03:49:02 -0300

    Add NLW Node JS
    Signed-off-by: CibeleG <cibelegoudinho13@gmail.com>

---
+++ server/src/services/AuthenticateUserService.ts
@@ -0,0 +1,44 @@
+import { getCustomRepository } from "typeorm";
+import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
+import { UsersRepositories } from "../repositories/UsersRepositories";
+
+interface IAuthenticateRequest {
+  email: string;
+  password: string;
+}
+
+class AuthenticateUserService {
+  async execute({ email, password }: IAuthenticateRequest) {
+    const usersRepositories = getCustomRepository(UsersRepositories);
+
+    const user = await usersRepositories.findOne({
+      email,
+    });
+
+    if (!user) {
+      throw new Error("Email/Password incorrect");
+    }
+
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new Error("Email/Password incorrect");
+    }
+
+    const token = sign(
+      {
+        email: user.email,
+      },
+      "13453b0618d540252b2aca9525c79304",
+      {
+        subject: user.id,
+        expiresIn: "1d",
+      }
+    );
+
+    return token;
+  }
+}
+
+export { AuthenticateUserService };
